f52c21efae19a42bb62464eab7574f423b37cafe
PiotrWojtalewicz <piowojt19@gmail.com>
2022-01-26 12:47:39 +0100

    Zadanie 2.2

--- src/House.java
+++ src/House.java
@@ -1,3 +1,15 @@
-public class House implements Building {
+public class House implements Building{
 
+    @Override
+    public double surface(double buildingSurface, double yardArea, double parkingSurface) {
+        return ;
+    }
+
+    @Override
+    public String adress(String locality, String Street, String houseNumber, String apartmentNumber, String postcode) {
+        return ;
+    }
 }
+
+
+
